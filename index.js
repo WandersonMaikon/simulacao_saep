7439361cc7044d64df6c22e11fa6623cd773e885
WandersonMaikon <wandersonmaikon@gmail.com>
2025-01-22 11:31:37 -0400

    Merge branch 'main' of https://github.com/WandersonMaikon/simulacao_saep

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
-const express = require('express');
-const mysql = require('mysql2');
-const bcrypt = require('bcryptjs');
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Configuração do banco de dados MySQL
 const db = mysql.createConnection({
@@ -15,16 +18,16 @@ const db = mysql.createConnection({
 
 db.connect((error) => {
   if (error) {
-    console.error('Erro ao conectar ao banco de dados:', error);
+    console.error("Erro ao conectar ao banco de dados:", error);
   } else {
-    console.log('Conectado ao MySQL com sucesso');
+    console.log("Conectado ao MySQL com sucesso");
   }
 });
 
 // Configuração do middleware
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(
   session({
@@ -35,79 +38,87 @@ app.use(
 );
 
 // Rota para exibir a página de login
-app.get('/login', (request, response) => {
-  response.render('login', { message: '' });
+app.get("/login", (request, response) => {
+  response.render("login", { message: "" });
 });
 
 // Rota para processar o login
-app.post('/login', (request, response) => {
+app.post("/login", (request, response) => {
   const { usuario, password } = request.body;
-  console.log('Dados recebidos:', request.body);
+  console.log("Dados recebidos:", request.body);
 
   db.query(
-    'SELECT * FROM usuario WHERE usuario = ?',
+    "SELECT * FROM usuario WHERE usuario = ?",
     [usuario],
     async (error, results) => {
       if (error) {
-        console.error('Erro na consulta SQL:', error);
-        return response.render('login', { message: 'Erro no sistema' });
+        console.error("Erro na consulta SQL:", error);
+        return response.render("login", { message: "Erro no sistema" });
       }
       if (results.length === 0) {
-        console.log('Usuário não encontrado:', usuario);
-        return response.render('login', { message: 'Usuário ou senha inválidos' });
+        console.log("Usuário não encontrado:", usuario);
+        return response.render("login", {
+          message: "Usuário ou senha inválidos",
+        });
       }
 
       const user = results[0];
-      console.log('Usuário encontrado:', user);
+      console.log("Usuário encontrado:", user);
 
       const passwordMatch = await bcrypt.compare(password, user.password);
-      console.log('Resultado da comparação de senha:', passwordMatch);
+      console.log("Resultado da comparação de senha:", passwordMatch);
 
       if (!passwordMatch) {
-        console.log('Senha incorreta para o usuário:', usuario);
-        return response.render('login', { message: 'Usuário ou senha inválidos' });
+        console.log("Senha incorreta para o usuário:", usuario);
+        return response.render("login", {
+          message: "Usuário ou senha inválidos",
+        });
       }
 
       request.session.user = user;
-      response.redirect('/dashboard');
+      response.redirect("/dashboard");
     }
   );
 });
 
 // Rota para exibir a página do dashboard
-app.get('/dashboard', (request, response) => {
+app.get("/dashboard", (request, response) => {
   if (!request.session.user) {
-    return response.redirect('/login');
+    return response.redirect("/login");
   }
-  response.render('dashboard', { username: request.session.user.usuario });
+  response.render("dashboard", { username: request.session.user.usuario });
 });
 
+app.use("/assets", express.static(path.join(__dirname, "views/assets")));
+
 // Rota para logout
-app.get('/logout', (request, response) => {
+app.get("/logout", (request, response) => {
   request.session.destroy();
-  response.redirect('/login');
+  response.redirect("/login");
 });
 
 // Rota para exibir a página de registro
-app.get('/registro', (request, response) => {
-  response.render('registro', { message: '' });
+app.get("/registro", (request, response) => {
+  response.render("registro", { message: "" });
 });
 
 // Rota para processar o registro de usuário
-app.post('/registro', async (request, response) => {
+app.post("/registro", async (request, response) => {
   const { username, password, email } = request.body;
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log('Senha hash gerada:', hashedPassword);
+  console.log("Senha hash gerada:", hashedPassword);
 
   db.query(
-    'INSERT INTO usuario (usuario, password, email) VALUES (?, ?, ?)',
+    "INSERT INTO usuario (usuario, password, email) VALUES (?, ?, ?)",
     [username, hashedPassword, email],
     (error) => {
       if (error) {
-        console.error('Erro ao inserir usuário:', error);
-        return response.render('registro', { message: 'Erro ao criar usuário' });
+        console.error("Erro ao inserir usuário:", error);
+        return response.render("registro", {
+          message: "Erro ao criar usuário",
+        });
       }
-      response.redirect('/login');
+      response.redirect("/login");
     }
   );
 });
