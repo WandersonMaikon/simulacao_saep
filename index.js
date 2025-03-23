d40d150e61682ee92ccf0ce997ef37b787b9531d
WandersonMaikon <wandersonmaikon@gmail.com>
2025-03-23 16:39:11 -0400

    Remover página 404 antiga e ajustar estilo da página de login para responsividade

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ app.use((req, res, next) => {
   res.status(404).render("404", { url: req.url });
 });
 
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
